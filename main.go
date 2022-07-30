4296932517b21dfa90b93019a8c012e28581fcb7
jnavacar <jnavacar1998@gmail.com>
2022-07-30 14:59:25 +0200

    get implemented

--- main.go
+++ main.go
@@ -1,35 +1,90 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"context"
+	"example/Go-API/handlers"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
-type Message struct {
-	Title   string `json:"Title"`
-	Content string `json:"content"`
-	Author  string `json:"author"`
-}
+// "context"
+// "fmt"
+// "log"
+// "time"
 
-type Messages []Message
-
-func allMessages(w http.ResponseWriter, r *http.Request) {
-	messages := Message{Title: "First Go endpoint", Content: "This is the first endpoint I create that returns some info", Author: "Jose"}
-	json.NewEncoder(w).Encode(messages)
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Homepage")
-}
-
-func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", allMessages)
-	log.Fatal(http.ListenAndServe(":8081", nil))
-}
+// "go.mongodb.org/mongo-driver/bson"
+// "go.mongodb.org/mongo-driver/mongo"
+// "go.mongodb.org/mongo-driver/mongo/options"
 
 func main() {
-	handleRequests()
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	ph := handlers.NewProducts(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", ph)
+
+	s := http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		ErrorLog:     l,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	signChan := make(chan os.Signal)
+	signal.Notify(signChan, os.Interrupt)
+	signal.Notify(signChan, os.Kill)
+
+	sig := <-signChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
+
+// client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://admin:admin@friendbook.ac2qv.mongodb.net/?retryWrites=true&w=majority"))
+// if err != nil {
+// 	log.Fatal(err)
+// }
+// ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+// err = client.Connect(ctx)
+// if err != nil {
+// 	log.Fatal(err)
+// }
+// defer client.Disconnect(ctx)
+
+// database := client.Database("friendbook-friends")
+// friendsCollection := database.Collection("friends")
+
+// cursor, err := friendsCollection.Find(ctx, bson.M{})
+// if err != nil {
+// 	log.Fatal(err)
+// }
+
+// // var friends []bson.M
+// // if err = cursor.All(ctx, &friends); err != nil {
+// // 	log.Fatal(err)
+// // }
+// // for _, friend := range friends {
+// // 	fmt.Println(friend["username"])
+// // }
+// defer cursor.Close(ctx)
+
+// for cursor.Next(ctx) {
+// 	var friend bson.M
+// 	if err = cursor.Decode(&friend); err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	fmt.Println(friend)
+// }
