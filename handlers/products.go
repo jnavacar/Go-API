a016a6e886afb0b702918613b18c71a292755b4c
jnavacar <jnavacar1998@gmail.com>
2022-08-04 02:40:54 +0200

    added check for get method

--- handlers/products.go
+++ handlers/products.go
@@ -14,7 +14,16 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-func (p *Products) ServeHTTP(rw http.ResponseWriter, h *http.Request) {
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
+
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts(rw http.ResponseWriter, h *http.Request) {
 	lp := data.GetProducts()
 	err := lp.ToJSON(rw) //encode instead of marshal because it is more efficient
 	if err != nil {
